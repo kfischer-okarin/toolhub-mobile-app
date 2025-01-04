f2c303ef629709c5af77885e3036b81eaabbd6d4
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-01-04 14:45:05 +0900

    Add input library

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,6 +1,14 @@
+require 'lib/input'
+
 require 'app/jni'
 
 def tick(args)
+  # Create an input
+  args.state.input ||= Input::Text.new(x: 100, y: 600, w: 300, focussed: true)
+
+  # Allow the input to process inputs and render text (render_target)
+  args.state.input.tick
+
   if args.inputs.mouse.click
     p JNI.game_activity_reference
     activity_class = JNI.get_object_class(JNI.game_activity_reference)
@@ -11,7 +19,18 @@ def tick(args)
     p result
   end
 
-  if args.inputs.keyboard.key_down.enter
-    puts "Hello from Ruby!"
-  end
+  center_x = 360
+  args.outputs.labels << {
+    x: center_x,
+    y: 720 - 32,
+    text: 'Click to call JNI method',
+    anchor_x: 0.5
+  }
+  args.outputs.primitives << args.state.input
+  # args.outputs.labels << {
+    # x: center_x,
+    # y: 720 - 64,
+    # text: "Last Input: #{args.inputs.keyboard.key_held.d}",
+    # anchor_x: 0.5
+  # }
 end
