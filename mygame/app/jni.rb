4c2bb54fb020f12d31912cc49de25c87b7d081d4
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-01-04 14:59:04 +0900

    Add some DSL

--- mygame/app/jni.rb
+++ mygame/app/jni.rb
@@ -1,7 +1,11 @@
 module JNI
   class << self
-    # Set via C extension
+    # Assigned in jni.c
     attr_reader :game_activity_reference
+
+    def game_activity
+      @game_activity ||= JavaObject.new(@game_activity_reference)
+    end
 
     # Defined in jni.c
     # def find_class(name) -> Reference
@@ -15,6 +19,8 @@ module JNI
   class ClassNotFound < Exception; end
   class NoSuchMethod < Exception; end
 
+  # Stores a JNI global reference internally
+  # Do not use this class directly
   class Reference
     attr_reader :type_name, :qualifier
 
@@ -23,11 +29,46 @@ module JNI
     end
   end
 
+  # Stores a JNI method or field ID internally
+  # Do not use this class directly
   class Pointer
     attr_reader :type_name, :qualifier
 
     def inspect
       "#<JNI::Pointer #{@type_name} #{@qualifier}>"
+    end
+  end
+
+  class JavaObject
+    attr_reader :reference
+
+    def initialize(reference)
+      @reference = reference
+    end
+
+    def java_class
+      @java_class ||= JavaClass.new(JNI.get_object_class(reference))
+    end
+
+    def inspect
+      "#<JNI::JavaObject #{@reference.qualifier}>"
+    end
+  end
+
+  class JavaClass
+    attr_reader :reference
+
+    def initialize(reference)
+      @reference = reference
+    end
+
+    def name
+      # qualifier has format 'class com.example.MyClass'
+      @name ||= @reference.qualifier.split.last
+    end
+
+    def inspect
+      "#<JNI::JavaClass #{name}>"
     end
   end
 end
--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -10,13 +10,13 @@ def tick(args)
   args.state.input.tick
 
   if args.inputs.mouse.click
-    p JNI.game_activity_reference
-    activity_class = JNI.get_object_class(JNI.game_activity_reference)
+    p JNI.game_activity
+    activity_class = JNI.game_activity.java_class
     p activity_class
-    method_id = JNI.get_static_method_id(activity_class, 'showTextInput', '(IIII)Z')
-    p method_id
-    result = JNI.call_static_boolean_method(activity_class, method_id, 100, 200, 300, 400)
-    p result
+    # method_id = JNI.get_static_method_id(activity_class, 'showTextInput', '(IIII)Z')
+    # p method_id
+    # result = JNI.call_static_boolean_method(activity_class, method_id, 100, 200, 300, 400)
+    # p result
   end
 
   center_x = 360
